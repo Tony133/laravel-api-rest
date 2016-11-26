e560fc03ee21cf77d28d92329ebaf56481a6db93
Tony133 <a.tripodi133@gmail.com>
2016-11-26 18:55:40 +0100

    Add middleware for authentication basic with username

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,5 +52,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'auth.basic.username' => \App\Http\Middleware\AuthenticateiByUsername::class,
     ];
 }
