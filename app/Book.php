beb8244bf27e795baf2d86af3a2cc47412f4b23b
Tony133 <a.tripodi133@gmail.com>
2019-09-07 23:50:54 +0200

    Added fields fillable on model Book

--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    protected $table = 'books';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title','price', 'author', 'editor',
+    ];
 }
