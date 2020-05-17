0e859077879a517b2bc9903be7e0ddde03c2d24f
Tony133 <a.tripodi133@gmail.com>
2020-05-17 16:25:47 +0200

    Added Resource Book

---
+++ app/Http/Resources/BookResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'price' => $this->price,
+            'author' => $this->author,
+            'editor'=> $this->editor,
+        ];
+    }
+}
